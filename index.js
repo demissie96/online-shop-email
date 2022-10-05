9be0aa831cf109374827fdfda5c2e49e96c9a210
Johannes Demissie <demissie.johannes@gmail.com>
2022-10-05 15:12:04 +0200

    Test nodemailer to send email.

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+const nodemailer = require("nodemailer");
 
 app.listen(port, () => {
-  console.log(`nodemailerProject is listening at http://localhost:${port}`)
-})
+  console.log(`nodemailerProject is listening at http://localhost:${port}`);
+  console.log(process.env.MAIL_USERNAME);
+
+  // Create a Transporter object
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
+
+  // Create a MailOptions Object
+  let mailOptions = {
+    from: `${process.env.MAIL_USERNAME}@gmail.com`,
+    to: "test-le2l6wnh2@srv1.mail-tester.com",
+    subject: "Nodemailer Project",
+    text: "Hi from your nodemailer project",
+  };
+
+  // Use the Transporter.sendMail method
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+});
